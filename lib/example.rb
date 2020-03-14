1a88c7739efbc6147be844ecc1af8c7d79673e1c
Soma Majumder <soma.creativeonline@gmail.com>
2020-03-14 20:04:49 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,14 +1,13 @@
 def say_hello_world_ten_times
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
-  puts "Hello World"
+  phrase = "Hello World"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
 
-say_hello_world_ten_times
